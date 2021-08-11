c053e1639f4ff000585a475018107ec1526d1dd1
Sarthak Jain <jainsarthak101@gmail.com>
2021-08-11 15:58:44 -0700

    satrt webcam

---
+++ webcam_backend.py
@@ -0,0 +1,14 @@
+import base64
+import html
+import io
+import time
+
+from IPython.display import display, Javascript
+from google.colab.output import eval_js
+import numpy as np
+from PIL import Image
+import cv2
+
+
+if __name__ == "__main__":
+    print("wdwkd")
