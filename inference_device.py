e65d4fb98cfee0f52e2fbfd2dc655b663903595b
Sarthak Jain <jainsarthak101@gmail.com>
2021-08-13 00:24:00 -0700

    create effdet and imports

--- inference_device.py
+++ inference_device.py
@@ -1,13 +1,59 @@
 import argparse
 import torch
+import torchvision
+import numpy as np
+import os
+import time
+import torch.nn.parallel
+from contextlib import suppress
 
+from effdet import create_model
+from effdet.data import resolve_input_config
+from timm.models.layers import set_layer_config
+from tqdm import tqdm
+from PIL import Image
+
+has_apex = False
+try:
+    from apex import amp
+    has_apex = True
+except ImportError:
+    pass
+
+has_native_amp = False
+try:
+    if getattr(torch.cuda.amp, 'autocast') is not None:
+        has_native_amp = True
+except AttributeError:
+    pass
+
+
+classes = ["Placeholder", "Apples", "Strawberry", "Peach", "Tomato", "Bad_Spots"]
 
 def set_device(input_device):
     global device
     device = torch.device(input_device)
     print("Device: {}".format(input_device))
 
-def infer_image(image_file_path, trained_model, distance_thresh, iou_thresh, voice_over = False):
+def load_torchvision_models(model_name):
+    if model_name == "ssdlite_mobilenet":
+        ssd_lite = torchvision.models.detection.ssdlite320_mobilenet_v3_large(pretrained_backbone=True, num_classes = len(classes))
+        ssd_lite.load_state_dict(torch.load("device/ssdlite_mobilenet/ssdlite_mobilenet_brain.pth"))
+        print("Loaded model weights for ssdlite_mobilenet")
+
+        return ssd_lite
+    else:
+        mobilenet_fasterrcnn = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn(pretrained=True)
+        mobilenet_fasterrcnn.roi_heads.box_predictor.cls_score.out_features = len(classes)
+        mobilenet_fasterrcnn.roi_heads.box_predictor.bbox_pred.out_features = 4 * (len(classes))
+
+        mobilenet_fasterrcnn.load_state_dict(torch.load("device/mobilenet_fasterrcnn/mobilenet_fasterrcnn_brain.pth"))
+        print("Loaded model weights for mobilenet_fasterrcnn")
+
+        return mobilenet_fasterrcnn
+
+
+def infer_image(image_file_path, trained_model, distance_thresh, iou_thresh, voice_over):
 
 
     torch_image = F.to_tensor(Image.open(image_file_path).convert("RGB")).unsqueeze(0).to(device)
@@ -80,7 +126,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "Infers on images and can give optional voice_over")
     parser.add_argument("image_file", type = str, help = "A file path to the image")
     parser.add_argument("--device", dest = "device", required = True, help = "name of device used")
-    parser.add_argument("--model_name", dest = "model_name", required = True, help = "name of model: [ED0, FRMN, SSD]")
+    parser.add_argument("--model_name", dest = "model_name", required = True, help = "name of models: [efficientdet_d0, mobilenet_fasterrcnn, ssdlite_mobilenet]")
     parser.add_argument("--confidence_thresh", dest = "confidence_thresh", required = True, help = "value for confidence thresholding \
     in nms")
     parser.add_argument("--iou_thresh", dest = "iou_thresh", required = True, help = "value of iou thresh in nms")
@@ -88,3 +134,16 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     set_device(args.device)
+
+    if args.voice_over:
+        voice_over = args.voice_over
+    else:
+        voice_over = False
+
+    if args.model_name == "efficientdet_d0":
+        results = stuff
+        # Change this
+    elif args.model_name == "mobilenet_fasterrcnn" or args.model_name = "ssdlite_mobilenet":
+        model = load_torchvision_models(args.model_name)
+    else:
+        raise ValueError("model_name can only be [efficientdet_d0, mobilenet_fasterrcnn, ssdlite_mobilenet]")
